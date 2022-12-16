d7e71dc864e5b9d0835c86cf8a654c814b3db918
zjoerg <82516176+zjoerg@users.noreply.github.com>
2022-12-16 11:37:19 +0100

    added source files for MauiCompat; fixed bug in FormsCameraView on iOS (deprated method AVCaptureDevice.DevicesWithMediaType in method RetrieveCameraDevice was replaced);

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.MauiCompat/Views/MediaElement/UriTypeConverter.shared.cs
@@ -0,0 +1,18 @@
+﻿using System;using Microsoft.Extensions.Logging;
+using Microsoft.Maui; using Microsoft.Maui.Controls; using Microsoft.Maui.Graphics; using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Xaml;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	[System.ComponentModel.TypeConverter(typeof(Uri))]
+	public class UriTypeConverter : System.ComponentModel.TypeConverter
+	{
+		public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+		{
+			if (value is not string valueStr) { throw new InvalidOperationException("Only typeof(string) allowed"); }
+			return string.IsNullOrWhiteSpace(valueStr) ? null : new Uri(valueStr, UriKind.RelativeOrAbsolute);
+		}
+	}
+}
